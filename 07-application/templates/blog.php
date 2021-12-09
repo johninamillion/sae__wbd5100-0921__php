d2afca0a36de83b365fd3f96af42e955ce5ad400
johninamillion <johninamio@gmail.com>
2021-12-09 21:00:59 +0100

    update templates and something i dont remember

--- 07-application/templates/blog.php
+++ 07-application/templates/blog.php
@@ -1,7 +1,19 @@
 
 <section>
     <h1>Blog</h1>
-    <pre>
-        <?php var_dump( get_blog_posts() ); ?>
-    </pre>
+    <?php
+        $blog_posts = get_blog_posts();
+
+        foreach ( $blog_posts as $post ) :
+            ?>
+
+            <article>
+                <h1><?= $post[ 'title' ] ?></h1>
+                <a href="#"><?= $post[ 'username' ] ?></a> | <span><?= date( 'd.m.Y - h:i', $post[ 'timestamp' ] ) ?></span>
+                <p><?= $post[ 'content' ] ?></p>
+            </article>
+
+            <?php
+        endforeach;
+    ?>
 </section>
